61c559b1ec6a351d43723ddfc0dbec98eb17858f
Garrett Kucinski <garrettryan@doejo.com>
2016-03-10 12:21:34 -0600

    adding right trap and fonts

--- front-page.php
+++ front-page.php
@@ -8,6 +8,10 @@
   </section>
 
   <section class="amenities-hero">
+    <article class="amenities--trap-article left">
+      <h2 class="amenities--trap-heading">Section Title</h2>
+      <p class="amenities--trap-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+    </article>
   </section>
 
   <section class="amenities-main">
