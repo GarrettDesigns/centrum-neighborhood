7692ab8a844e877a17b18fe9c0b7a63b3fca9936
Garrett Kucinski <garrettryan@doejo.com>
2016-03-10 02:26:04 -0600

    finished rough prototype of full centrum site

--- front-page.php
+++ front-page.php
@@ -11,6 +11,16 @@
   </section>
 
   <section class="amenities-main">
+    <ul class="amenities-main--list">
+      <li class="amenities-main--heading">Amenities</li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/house.svg" alt="a house icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/barbell.svg" alt="a barbell icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/swimming.svg" alt="a swimming icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/washer.svg" alt="a washer icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/bicycle.svg" alt="a bicycle icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/envelope.svg" alt="an envelope icon"></li>
+      <li class="amenities-main--list-item"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/coffee.svg" alt="a coffee icon"></li>
+    </ul>
   </section>
 
   <section class="neighborhood-hero">
@@ -32,48 +42,167 @@
   </section>
 
   <section class="features">
-    <h2 class="feetures-list--heading">Features</h2>
-    <ul class="features-list">
-      <li class="features-list--item">State-of-the-art fitness center</li>
-      <li class="features-list--item">Party room with kitchenette, internet café
-and billiards table</li>
-      <li class="features-list--item">Outdoor terrace with lounge seating, private
-BBQ cooking station and fire pit</li>
-      <li class="features-list--item">Pressbox lockers</li>
-      <li class="features-list--item">Luxer One package storage room</li>
-      <li class="features-list--item">Bike parking and bike maintenance
-equipment for urban cyclists</li>
-      <li class="features-list--item">The building will have 7 interior and 1
-exterior parking spaces. The outside parking
-space will be reserved for a “car-sharing”
-vehicle&mdash;Enterprise CarShare or Zipcar&mdash;
-for eco-friendly travel</li>
-      <li class="features-list--item">Two full service elevators
+    <div class="features--trapezoid">
+      <article class="features--trap-article right">
+        <h2 class="features--trap-heading">Section Title</h2>
+        <p class="features--trap-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+      </article>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/feature-trapezoid.svg" alt="blue trapezoid">
+    </div>
+    <article class="features-list">
+    <h2 class="features-list--heading">Features</h2>
+    <ul>
+      <li class="features-list--column">
+        <ul>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> State-of-the-art fitness center</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Party room with kitchenette, internet café
+    and billiards table</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Outdoor terrace with lounge seating, private
+    BBQ cooking station and fire pit</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Pressbox lockers</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Luxer One package storage room</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Bike parking and bike maintenance
+    equipment for urban cyclists</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> The building will have 7 interior and 1
+    exterior parking spaces. The outside parking
+    space will be reserved for a “car-sharing”
+    vehicle&mdash;Enterprise CarShare or Zipcar&mdash;
+    for eco-friendly travel</p></li>
+        </ul>
+      </li>
+      <li class="features-list--column">
+        <ul>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Two full service elevators
+          </li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> A loading dock is provided off the
+    alley to the west for moving and
+    delivery of large items
+  </p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Professionally managed building</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Building will be Energy Star certified</p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> SNew Bow Truss Coffee location on
+    the ground floor
+  </p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Additional Restaurant/Retail users on
+    ground floor retail space
+  </p></li>
+          <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Building is a Transportation Oriented
+    Development (TOD) taking advantage of
+    its proximity to public transportation and
+    Paulina Brown Line stop</p></li>
+  </ul>
 </li>
-      <li class="features-list--item">A loading dock is provided off the
-alley to the west for moving and
-delivery of large items
-</li>
-      <li class="features-list--item">Professionally managed building</li>
-      <li class="features-list--item">Building will be Energy Star certified</li>
-      <li class="features-list--item">SNew Bow Truss Coffee location on
-the ground floor
-</li>
-      <li class="features-list--item">Additional Restaurant/Retail users on
-ground floor retail space
-</li>
-      <li class="features-list--item">Building is a Transportation Oriented
-Development (TOD) taking advantage of
-its proximity to public transportation and
-Paulina Brown Line stop</li>
-      <li class="features-list--item">Building recycling program</li>
-      <li class="features-list--item">Energy efficient windows and expansive
-glass for natural daylight</li>
-      <li class="features-list--item">Non-smoking building</li>
-      <li class="features-list--item">Free Wi-Fi in amenity areas</li>
-      <li class="features-list--item">Acoustically treated windows on the
-north side to help mitigate L noise</li>
-      <li class="features-list--item">Pet-Friendly</li>
+<li class="features-list--column">
+  <ul>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Building recycling program</p></li>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Energy efficient windows and expansive
+glass for natural daylight</p></li>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Non-smoking building</p></li>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Free Wi-Fi in amenity areas</p></li>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Acoustically treated windows on the
+north side to help mitigate L noise</p></li>
+      <li class="features-list--item"><i class="fa fa-check-circle-o"></i><p> Pet-Friendly</p></li>
+    </ul>
+  </li>
+</ul>
+</article>
+  </section>
+
+  <section class="interior-view-one">
+    <div class="interior-view-one--trapezoid">
+      <article class="features--trap-article left">
+        <h2 class="features--trap-heading">Section Title</h2>
+        <p class="features--trap-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+      </article>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/interior-trapezoid.svg" alt="a trapezoid">
+    </div>
+  </section>
+
+  <section class="interior-view-two">
+
+  </section>
+
+  <section class="current-availability">
+    <h2 class="current-availability--heading">Current Availability</h2>
+    <ul class="bedroom-options">
+      <li class="bedroom-options--label">Number of Bedrooms</li>
+      <li class="bedroom-options--item">All</li>
+      <li class="bedroom-options--item">1</li>
+      <li class="bedroom-options--item">2</li>
+    </ul>
+    <ul class="availability-table">
+      <li class="availability-table--head">
+        <li class="availability-table--head-cell">Apartment</li>
+        <li class="availability-table--head-cell">Bedrooms</li>
+        <li class="availability-table--head-cell">Bathrooms</li>
+        <li class="availability-table--head-cell">Size</li>
+        <li class="availability-table--head-cell">Rent</li>
+        <li class="availability-table--head-cell">Floorplan</li>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
+      <li class="availability-table--row">
+        <span class="availability-table--cell">100</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">2</span>
+        <span class="availability-table--cell">1541 sq ft</span>
+        <span class="availability-table--cell">$3,200,000</span>
+        <span class="availability-table--cell">View</span>
+      </li>
     </ul>
   </section>
 <?php endwhile; ?>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,10 @@
 
-<div class="page-header">
+<header class="page-header">
   <span class="color-overlay">
   <section class="page-header--content">
+
+    <span class="menu-icon"></span>
+
     <?php
       $menu_args = array(
         'menu_class' => 'main-menu',
@@ -11,16 +14,15 @@
       wp_nav_menu($menu_args);
     ?>
 
-    <span class="menu-icon"></span>
-
     <ul class="info-links">
       <li class="info-links--item"><a class="info-links--inner error buttons" href="#">contact us</a></li>
       <li class="info-links--item"><a class="info-links--inner primary buttons" href="#">check availability</a></li>
     </ul>
 
+  </section>
+
     <div class="site-logo--container">
       <img class="site-logo" src="<?php echo get_template_directory_uri(); ?>/dist/images/centrum-lakeview-logo.png" alt="Centrum Lakview Logo">
     </div>
-  </section>
 </span>
-</div>
+</header>
