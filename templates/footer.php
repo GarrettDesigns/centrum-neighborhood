7467528e3659d07c663872dcf910ae2c835959ae
Garrett Kucinski <garrettryan@doejo.com>
2016-05-03 17:28:45 -0500

    added footer info to wp, hooked up logos

--- templates/footer.php
+++ templates/footer.php
@@ -1,23 +1,31 @@
 <ul class="footer-info">
-  <li class="footer-info--list">
-    <ul id="contact">
-      <!-- <li class="footer-info--heading">contact us</li> -->
-      <li class="footer-info--address"><span class="footer-info--address-item">312-632-0734</span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item">3400 North Lincoln Avenue</span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item">Chicago, Illinois 60657</span></li>
-    </ul>
-  </li>
-  <li class="footer-info--list">
-    <ul>
-      <li class="footer-info--social-media"><a href="https://www.facebook.com/Live-Centrum-1612416582412368" target="_blank">facebook</a></li>
-      <li class="footer-info--social-media"><a href="http://www.twitter.com/live_centrum" target="_blank">twitter</a></li>
-      <li class="footer-info--social-media"><a href="http://www.instagram.com/livecentrum" target="_blank">instagram</a></li>
+	<li class="footer-info--list">
+		<ul id="contact">
+			<!-- <li class="footer-info--heading">contact us</li> -->
+			<?php
+				$phone_number = get_field( 'phone_number' );
+				$address = get_field( 'address' );
+
+				$facebook = get_field( 'facebook_link' );
+				$twitter = get_field( 'twitter_link' );
+				$instagram = get_field( 'instagram_link' );
+			?>
+			<li class="footer-info--address"><span class="footer-info--address-item"><?php echo esc_html( $phone_number ); ?></span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item"><?php echo esc_html( $address ); ?></span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item">Chicago, Illinois 60657</span></li>
+		</ul>
+	</li>
+	<li class="footer-info--list">
+		<ul>
+			<li class="footer-info--social-media"><a href="<?php echo esc_url( $facebook ); ?>" target="_blank">facebook</a></li>
+			<li class="footer-info--social-media"><a href="<?php echo esc_url( $twitter ); ?>" target="_blank">twitter</a></li>
+			<li class="footer-info--social-media"><a href="<?php echo esc_url( $instagram ); ?>" target="_blank">instagram</a></li>
 			<li class="footer-info--item copyright">&copy; LiveCentrum 2016</li>
-    </ul>
-  </li>
-  <!-- <li class="footer-info--list">
-    <ul>
-      <li class="footer-info--item">privacy policy</li>
-      <li class="footer-info--item">terms of service</li>
-      <li class="footer-info--item">&copy; Centrum Living 2016</li>
-    </ul>
-  </li> -->
+		</ul>
+	</li>
+	<!-- <li class="footer-info--list">
+		<ul>
+			<li class="footer-info--item">privacy policy</li>
+			<li class="footer-info--item">terms of service</li>
+			<li class="footer-info--item">&copy; Centrum Living 2016</li>
+		</ul>
+	</li> -->
 </ul>
