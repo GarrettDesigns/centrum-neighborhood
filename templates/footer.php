9966b557f3b5914532cc9720026c60d65a94d94e
Garrett Kucinski <garrettryan@doejo.com>
2016-05-02 20:05:18 -0500

    fixing the footer styling after adding social media

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,6 @@
     <ul id="contact">
       <!-- <li class="footer-info--heading">contact us</li> -->
       <li class="footer-info--address"><span class="footer-info--address-item">312-632-0734</span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item">3400 North Lincoln Avenue</span> <span class="footer-pipe">&nbsp;&nbsp;|&nbsp;&nbsp;</span> <span class="footer-info--address-item">Chicago, Illinois 60657</span></li>
-      <li class="footer-info--item">&copy; LiveCentrum 2016</li>
     </ul>
   </li>
   <li class="footer-info--list">
@@ -11,6 +10,7 @@
       <li class="footer-info--social-media"><a href="https://www.facebook.com/Live-Centrum-1612416582412368">facebook</a></li>
       <li class="footer-info--social-media"><a href="www.twitter.com/live_centrum">twitter</a></li>
       <li class="footer-info--social-media"><a href="www.instagram.com/livecentrum">instagram</a></li>
+			<li class="footer-info--item copyright">&copy; LiveCentrum 2016</li>
     </ul>
   </li>
   <!-- <li class="footer-info--list">
