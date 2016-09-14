c53af22639c4b8f83d0d96e7a918d7c7272c1f50
Garrett Kucinski <garrettryan@doejo.com>
2016-09-14 16:54:54 -0500

    added contact7 form

--- templates/content-contact-us.php
+++ templates/content-contact-us.php
@@ -1,5 +1,6 @@
 <div class="contact-form--container">
     <h1 class="update-headline">stay up to date</h1>
     <p class="update-flavor-text">To stay connected with Centrum Bucktown, please register below to become a VIP and receive information first.</p>
-    <?php dynamic_sidebar( 'contact-us-form' ); ?>
+    <?php // dynamic_sidebar( 'contact-us-form' ); ?>
+    <?php echo do_shortcode( '[contact-form-7 id="696" title="Wicker Park Lead Generation Form"]' ); ?>
 </div>
